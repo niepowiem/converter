4f53bc626b58ccc83c2e9f13744518d7bf35504d
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2025-06-04 20:51:01 +0200

    Update installResources.ps1

--- installResources.ps1
+++ installResources.ps1
@@ -6,8 +6,8 @@ pip install pyyaml
 # Instalacja biblioteki do obsługi plików .xml
 pip install xmltodict
 
-# Instalacja PyQt5 (jeśli planujesz UI w dalszym etapie)
-# pip install pyqt5
+# Instalacja PyQt5
+pip install pyqt5
 
 # Instalacja pyinstaller do budowania pliku .exe
 pip install pyinstaller
