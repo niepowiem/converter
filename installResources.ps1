c39c6d48b84869ae25ae6f018dbe6a1bb8491607
Bartosz Zieliński <59732574+niepowiem@users.noreply.github.com>
2025-06-04 20:05:35 +0200

    Wykonanie Task0

---
+++ installResources.ps1
@@ -0,0 +1,13 @@
+# Skrypt PowerShell do instalacji wymaganych bibliotek Pythona
+
+# Instalacja PyYAML do obsługi plików .yml/.yaml
+pip install pyyaml
+
+# Instalacja biblioteki do obsługi plików .xml
+pip install xmltodict
+
+# Instalacja PyQt5 (jeśli planujesz UI w dalszym etapie)
+# pip install pyqt5
+
+# Instalacja pyinstaller do budowania pliku .exe
+pip install pyinstaller
